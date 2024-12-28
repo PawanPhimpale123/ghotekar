fba66558e84a7577b4e8113c95cde6656f963cef
PawanPhimpale123 <pavanfimpale143@gmail.com>
2024-12-28 14:44:28 +0530

    Update hello1.java

--- hello1.java
+++ hello1.java
@@ -1,6 +1,8 @@
 class hello1{
   public static void main(String args[]){
-    int a=5+10;
-    System.out.println("sum"+a);
+    for(int i=1;i<=10;i++)
+      {
+        System.out.println("1 to 10"+i);
+      }
   }
 }
